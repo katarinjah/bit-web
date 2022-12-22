4f50116c1d2fe2dcdd82c867e27d954ea8b17b2c
katarinjah <katarina.d.262@gmail.com>
2022-12-22 23:58:52 +0100

    Partially handled task 5 from Events _ Exercises

---
+++ DOM/events/event-propagation.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var widePics = document.getElementsByClassName("wideimg");
+for (var i = 0; i < widePics.length; i++) {
+    widePics[i].setAttribute("width", "300px");
+    widePics[i].setAttribute("height", "200px");
+}
+
+var imgNodes = document.querySelectorAll("img");
+
+imgNodes.forEach(item => {
+    item.addEventListener('click', event => {
+        item.classList.add("border");
+    })
+})
+
+  
+
+            
+
