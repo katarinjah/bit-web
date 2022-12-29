a19bd041fc2418bdb1382cbd7b8ce7a914961007
katarinjah <katarina.d.262@gmail.com>
2022-12-29 21:17:07 +0100

    Corrected Soccer Player task and started Super Mario task

--- DOM/events/soccer-player/event-listeners-soccer.js
+++ DOM/events/soccer-player/event-listeners-soccer.js
@@ -2,11 +2,18 @@
 
 var field = document.querySelector(".container");
 var player = document.querySelector(".player");
+var button = document.querySelector(".button");
 
-field.addEventListener("click", function() {
+var move = function() {
     var x = event.clientX;
     var y = event.clientY;
     player.style.position = "absolute";
     player.style.left = x + "px";
     player.style.top = y + "px";
-})
+}
+
+field.addEventListener("click", move);
+
+button.addEventListener("click", function() {
+  field.removeEventListener("click", move);
+});
---
+++ DOM/events/super-mario/event-listeners-mario.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var mario = document.getElementById("mario");
+var runningMario = document.getElementById("running-mario");
+var container = document.querySelector(".container");
+
+function makeMarioRun(event) {
+
+}
