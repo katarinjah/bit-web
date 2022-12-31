098ff983cf2c611cbdabcf173025b5d501e82071
katarinjah <katarina.d.262@gmail.com>
2022-12-31 07:35:02 +0100

    Completed the Super Mario task

--- DOM/events/super-mario/event-listeners-mario.js
+++ DOM/events/super-mario/event-listeners-mario.js
@@ -2,19 +2,21 @@
 
 var mario = document.getElementById("mario");
 var runningMario = document.getElementById("running-mario");
+var container = document.querySelector(".container");
 
 document.addEventListener("keydown", function(event) {
-    if (event.keyCode === 39) {
-      mario.style.display = "none";
-      runningMario.style.display = "block";
-    }
-  });
+  if (event.key === "ArrowRight") {
+    mario.style.display = "none";
+    runningMario.style.display = "block";
+    var position = parseInt(container.style.backgroundPosition) || 0;
+    container.style.backgroundPosition = (position - 10) + "px";
+  }
+})
 
-  document.addEventListener("keyup", function(event) {
-    if (event.keyCode === 39) {
-      mario.style.display = "block";
-      runningMario.style.display = "none";
-    }
-  });
-
+document.addEventListener("keyup", function(event) {
+  if (event.key === "ArrowRight") {
+    mario.style.display = "block";
+    runningMario.style.display = "none";  
+  }
+})
   
